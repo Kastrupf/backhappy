6d980e81a21288e9a85c1fc2a66ea075e44bfab6
kastrupf <kastrupf@gmail.com>
2020-10-17 20:58:12 +0200

    creation de la connection avec la base de données

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express';
 
+import './database/connection';
+
 
 const app = express();
+
+app.use(express.json());
 
 app.get('/test', (request, response) => {
     return response.json ({ message: 'Hello Word' });
