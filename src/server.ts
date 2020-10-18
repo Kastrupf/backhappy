df5d04a9a463c325a4dacac15e758bf1d8e554ed
kastrupf <kastrupf@gmail.com>
2020-10-18 14:55:53 +0200

    accès aux images à partir de l'adresse dans l'API quand la liste de foyers est demandée

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
-import './database/connection';
+import path from 'path';
 
+import './database/connection';
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
 
 // localhost:3333
 app.listen(3333);
